c4bd65f367d1e80db549fab2dd5ea4546bc33648
quzor <egor.khantakov@gmail.com>
2024-05-29 13:08:31 +0700

    add transitions

--- fsm/graph_build/graph_build.py
+++ fsm/graph_build/graph_build.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QAction, QToolBar, QGraphicsView, QGraphicsScene,
                              QGraphicsItemGroup, QGraphicsEllipseItem, QGraphicsTextItem, QGraphicsItem,
-                             QGraphicsLineItem)
-from PyQt5.QtGui import QFont, QPen, QPainter, QColor
-from PyQt5.QtCore import Qt, QRectF, QLineF
+                             QGraphicsLineItem, QInputDialog)
+from PyQt5.QtGui import QFont, QPen, QPainter, QColor, QPolygonF
+from PyQt5.QtCore import Qt, QRectF, QLineF, QPointF
 
 
 class StateItem(QGraphicsItemGroup):
@@ -46,11 +46,67 @@ class StateItem(QGraphicsItemGroup):
             self.toggle_final()
         super().mouseDoubleClickEvent(event)
 
+    # def itemChange(self, change, value):
+    #     if change == QGraphicsItem.ItemPositionChange:
+    #         for arrow in self.scene().items():
+    #             if isinstance(arrow, TransitionArrow) and (arrow.start_item is self or arrow.end_item is self):
+    #                 arrow.update_position()
+    #     return super().itemChange(change, value)
+
+    def mousePressEvent(self, event):
+        # Реализация выбора с поддержкой множественного выбора
+        if event.modifiers() & Qt.ControlModifier:
+            self.setSelected(not self.isSelected())
+        else:
+            super(StateItem, self).mousePressEvent(event)
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.ItemPositionChange:
+            self.updateConnectedTransitions()
+        return super().itemChange(change, value)
+
+    def updateConnectedTransitions(self):
+        # Обновляем положение всех связанных переходов
+        for item in self.scene().items():
+            if isinstance(item, TransitionArrow) and (item.start_item == self or item.end_item == self):
+                item.update_position()
+
+
+class TransitionArrow(QGraphicsLineItem):
+    def __init__(self, start_item, end_item, symbol):
+        super().__init__()
+        self.start_item = start_item
+        self.end_item = end_item
+        self.symbol = symbol
+        self.setPen(QPen(Qt.black, 2))
+        self.setZValue(-1)  # Устанавливаем стрелки под состояниями
+
+        # Добавление текста для символа перехода
+        self.text = QGraphicsTextItem(symbol, self)
+        self.text.setFont(QFont("Arial", 10))
+        self.update_position()
+
+    def update_position(self):
+        line = QLineF(self.start_item.scenePos(), self.end_item.scenePos())
+        self.setLine(line)
+
+        # Позиционирование текста перехода в середине стрелки
+        self.text.setPos(
+            line.pointAt(0.5) - QPointF(self.text.boundingRect().width() / 2, self.text.boundingRect().height()))
+
+    def paint(self, painter, option, widget):
+        super().paint(painter, option, widget)
+        # Рисуем стрелку в конце линии
+        end_point = self.line().p2()
+        arrow_head = QPolygonF([end_point + QPointF(-10, 10), end_point + QPointF(-10, -10), end_point])
+        painter.drawPolygon(arrow_head)
+
 
 class FSMEditor(QMainWindow):
     def __init__(self):
         super().__init__()
         self.state_id = 0
+        self.selected_states = []
         self.initUI()
 
     def initUI(self):
@@ -76,12 +132,43 @@ class FSMEditor(QMainWindow):
 
         addAction.triggered.connect(self.addState)
 
+        addTransitionAction = QAction('Add Transition', self)
+        self.toolbar.addAction(addTransitionAction)
+        addTransitionAction.triggered.connect(self.addTransition)
+
+        self.scene.selectionChanged.connect(self.updateSelectedStates)
+
     def addState(self):
         name = "q" + str(self.state_id)
         state = StateItem(name)
         self.scene.addItem(state)
         state.setPos(self.view.mapToScene(self.view.viewport().rect().center()))
         self.state_id += 1
+
+    def updateSelectedStates(self):
+        current_selection = self.scene.selectedItems()
+        if len(current_selection) == 2 and all(isinstance(item, StateItem) for item in current_selection):
+            self.selected_states = current_selection
+        elif len(current_selection) > 2:
+            # Очищаем выбор, если выбрано больше двух элементов
+            for item in current_selection:
+                if item not in self.selected_states:
+                    item.setSelected(False)
+
+    def addTransition(self):
+        if len(self.selected_states) == 2:
+            symbol, ok = QInputDialog.getText(self, 'Transition Symbol', 'Enter symbol for the transition:')
+            if ok and symbol:
+                transition = TransitionArrow(self.selected_states[0], self.selected_states[1], symbol)
+                self.scene.addItem(transition)
+                self.selected_states[0].setSelected(False)
+                self.selected_states[1].setSelected(False)
+                self.selected_states = []
+
+    def itemMoved(self):
+        for item in self.scene.items():
+            if isinstance(item, TransitionArrow):
+                item.update_position()
 
 
 if __name__ == '__main__':
